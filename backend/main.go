b30e6aa8abf10bf7510b564fcd872bbf9c5f6341
hiroj <hirojchoudhury@gmail.com>
2026-02-06 02:01:03 +0530

    Frontend folder (in react) added for go, added middlewares

--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"example.com/rest-api/db"
+	"example.com/rest-api/middlewares"
 	"example.com/rest-api/routes"
 	"github.com/gin-gonic/gin"
 )
@@ -9,7 +10,7 @@ import (
 func main() {
 	db.InitDB()
 	server := gin.Default()
-
+	server.Use(middlewares.CORSMiddleware())
 	routes.RegisterRoutes(server)
 
 	server.Run(":8080") // localhost:8080
---
+++ backend/middlewares/cors.go
@@ -0,0 +1,23 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "false")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Origin")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+
+		if c.Request.Method == http.MethodOptions {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
